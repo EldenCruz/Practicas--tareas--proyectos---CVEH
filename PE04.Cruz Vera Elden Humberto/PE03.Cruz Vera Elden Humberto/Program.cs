27a658e5c835efeedca59de8e29a6c4204de0a48
Elden Humberto Cruz Vera <elden.cruz17@tectijuana.edu.mx>
2018-09-24 23:24:01 -0700

    Practicas Evaluativas
    sorry por la tardanza

---
+++ PE04.Cruz Vera Elden Humberto/PE03.Cruz Vera Elden Humberto/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE03.Cruz_Vera_Elden_Humberto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Num1 = 0;
+            int Num2 = 0;
+            int Num3 = 0;
+            int Minimo = 0;
+
+            try
+            {
+                Console.Write("Ingrese el primer número: ");
+                Num1 = Int16.Parse(Console.ReadLine());
+
+                Console.Write("Ingrese el primer número: ");
+                Num2 = Int16.Parse(Console.ReadLine());
+
+                Console.Write("Ingrese el primer número: ");
+                Num3 = Int16.Parse(Console.ReadLine());
+
+                if (Num1 < Num2)
+                {
+                    Minimo = Num1;
+                    if (Num1 < Num3)
+                        Minimo = Num1;
+                    else
+                        Minimo = Num3;
+                }
+                else
+                {
+                    Minimo = Num2;
+                    if (Num2 < Num3)
+                        Minimo = Num2;
+                    else
+                        Minimo = Num3;
+                }
+                Console.Write("\nEl valor minimo es: {0}", Minimo);
+            }
+
+            catch (Exception x)
+            {
+                Console.WriteLine(x.Message);
+            }
+
+            finally
+            {
+                Console.Write("\nPresione una tecla para salir ");
+                Console.ReadKey();
+            }
+        }
+    }
+}
